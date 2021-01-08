6622e32de9e7532b999eda222b224e84f3ffaf36
Christian Jung <christian.jung@reaze.com>
2021-01-08 15:51:46 +0100

    feat: first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0f748a791a913dc3e5f55949e1898fab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '47902dd9-339d-4e3f-ac53-b6dd12470c62'
+};
